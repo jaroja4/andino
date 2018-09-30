5ef691c68c8447fe1f2ed9961acd7c3d26319f1f
Car11 <carlos.echc11@gmail.com>
2018-09-30 12:31:16 -0600

    01

--- class/globals.php
+++ class/globals.php
@@ -1,8 +1,9 @@
 <?php
 class Globals {
     const app = 'ANDINO';
+    const configFile= "../../../ini/config.ini";
+    const certDir= "../../../CU/";
     const version= '1.0';
-    const cssversion= "1.0";
-    static $config="";    
+    const cssversion= "1.0"; 
 }
 ?>

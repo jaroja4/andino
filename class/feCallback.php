ece5f82678c26694e1b3cca8f4e4ce732ecbab01
Car11 <carlos.echc11@gmail.com>
2018-10-06 21:43:21 -0600

    fix13

--- class/feCallback.php
+++ class/feCallback.php
@@ -20,16 +20,17 @@
             $entidad->username = 'cpf-01-1187-0763@stag.comprobanteselectronicos.go.cr';
             $entidad->password = '9zgr)L#szb^Z=%*+;%c|';
             $_SESSION['API'] = $entidad;
-            error_log("[info] session username: " . $_SESSION['API']->username);
-            error_log("[info] session pw: " . $_SESSION['API']->password);
+            //
             if(!$entidad->APILogin()){
                 error_log("[ERROR] api token (-501): No es posible generar token de api");
                 exit;
             }
             // consulta de comprobantes.
-            foreach ($data as $key => $value){
-                error_log("[INFO] consulta factura: " . $value['clave']);
+            foreach ($data as $key => $value){                
                 $_SESSION['API']->clave = $value['clave'];                
+                error_log("[INFO] consulta factura: " . $_SESSION['API']->clave);
+                error_log("[info] session username: " . $_SESSION['API']->username);
+                error_log("[info] session pw: " . $_SESSION['API']->password);
                 facturaElectronica::APIConsultaComprobante();
                 //checkComprobante($value['clave']);
             }

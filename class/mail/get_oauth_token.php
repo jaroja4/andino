1842ab14d6079bc3a7c5aae8dd0419d6210175d8
Car11 <carlos.echc11@gmail.com>
2019-01-20 21:57:36 -0600

    oauth

--- class/mail/get_oauth_token.php
+++ class/mail/get_oauth_token.php
@@ -1,3 +1,3 @@
-$redirectUri = 'http://faccr.storylabscr.com/class/mail/get_oauth_token.php';
+$redirectUri = 'https://faccr.com/class/mail/get_oauth_token.php';
 $clientId = '403994346860-otmp39fqt5sb4s6ks969fn1d7qifcvfd.apps.googleusercontent.com';
 $clientSecret = 'raCmb6XJjUErJ4XSwPuOvaLF';

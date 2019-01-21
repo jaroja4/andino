93b7673585d905d875bc3f1c8dfd04e22bc56b3a
Car11 <carlos.echc11@gmail.com>
2019-01-20 23:24:27 -0600

    08

--- class/mail/get_oauth_token.php
+++ class/mail/get_oauth_token.php
@@ -1,5 +1,4 @@
-<?php
+
 $redirectUri = 'https://facturaelectronica.faccr.com/class/mail/get_oauth_token.php';
 $clientId = '403994346860-otmp39fqt5sb4s6ks969fn1d7qifcvfd.apps.googleusercontent.com';
 $clientSecret = 'wgVcFFueIkj2Wo9tuW0WM07n';
-?>

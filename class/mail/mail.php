8af4e5492531a24ab74d97a10559af4f29d0cec6
Car11 <carlos.echc11@gmail.com>
2019-01-21 01:42:54 -0600

    fixphpInvoice

--- class/mail/mail.php
+++ class/mail/mail.php
@@ -1,15 +1,15 @@
 <?php
-    require 'Exception.php';
-    require 'PHPMailer.php';
-    require 'SMTP.php';
-    require 'OAuth.php';
+    // require 'Exception.php';
+    // require 'PHPMailer.php';
+    // require 'SMTP.php';
+    // require 'OAuth.php';
     setlocale(LC_ALL,"es_ES");
     mb_internal_encoding('UTF-8');
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\OAuth;
-    use PHPMailer\PHPMailer\Exception;
-    use League\OAuth2\Client\Provider\Google;
+    // use PHPMailer\PHPMailer\PHPMailer;
+    // use PHPMailer\PHPMailer\OAuth;
+    // use PHPMailer\PHPMailer\Exception;
+    // use League\OAuth2\Client\Provider\Google;
 
     require 'vendor/autoload.php';
 
@@ -31,6 +31,10 @@
             $phpmailer  = new PHPMailer(true);   // Passing `true` enables exceptions
             $phpmailer->CharSet = "UTF-8";
             $phpmailer->MailerDebug = false;
+            $phpmailer->oauthUserEmail = "carlos.echc11@gmail.com";
+            $phpmailer->oauthClientId = "403994346860-otmp39fqt5sb4s6ks969fn1d7qifcvfd.apps.googleusercontent.com";
+            $phpmailer->oauthClientSecret = "wgVcFFueIkj2Wo9tuW0WM07n";
+            $phpmailer->oauthRefreshToken = "1/5FtM6mCpMNnW2feYcjdvgb-erRZuTj0JzWTSaabNrVQ";
             try {
                 //Server settings
                 $phpmailer->Username = $this->email_user;
@@ -40,16 +44,36 @@
                 $phpmailer->Host = $this->email_Host;
                 $phpmailer->SMTPSecure = $this->email_SMTPSecure;
                 $phpmailer->Port = (int)$this->email_Port;
-                $phpmailer->SMTPAuth = $this->email_SMTPAuth;
-    
-            
+
                 $phpmailer->IsSMTP(); // use SMTP Gmail
+                $phpmailer->SMTPDebug = 2;
                 $phpmailer->SMTPAuth = true;
+                $phpmailer->AuthType = 'XOAUTH2';
+                $provider = new Google(
+                    [
+                        'clientId' => $phpmailer->oauthClientId,
+                        'clientSecret' => $phpmailer->oauthClientSecret,
+                    ]
+                );
+    
                 $phpmailer->setFrom($phpmailer->Username,$this->email_from_name);
 
                 foreach ($this->email_array_address_to as $address_to) {
-                    $phpmailer->AddAddress($address_to); // recipients email    
-                }                   
+                    $phpmailer->AddAddress($address_to); // recipients email
+
+                    $phpmailer->setOAuth(
+                        new OAuth(
+                            [
+                                'provider' => $provider,
+                                'clientId' => $phpmailer->oauthClientId,
+                                'clientSecret' => $phpmailer->oauthClientSecret,
+                                'refreshToken' =>  $phpmailer->oauthRefreshToken,
+                                'userName' => $address_to,
+                            ]
+                        )
+                    );
+                }
+
                 $phpmailer->Subject = $this->email_subject;	
 
                 foreach ($this->email_addAttachment as $Attachment) {
